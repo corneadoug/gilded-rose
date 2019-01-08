99a613710ae7c6a15da2ae702c846e58d95ddf2c
Damien CORNEAU <corneadoug@gmail.com>
2019-01-08 01:19:27 +0100

    Add checks in Utils

--- front/src/utils.js
+++ front/src/utils.js
@@ -1,15 +1,21 @@
 export function itemMatchesFilter(item, filter) {
-    let isAMatch = true;
+  let isAMatch = true;
 
+  // TODO - Try to find a better way to handle those checks, Typescript?
+  if (item && item.name && item.hasOwnProperty('quality') && filter &&
+    filter.hasOwnProperty('name') && filter.hasOwnProperty('minQuality') && filter.hasOwnProperty('maxQuality')) {
     // Check for name filter
     if (filter.name !== '' && !item.name.toLowerCase().includes(filter.name.toLowerCase())) {
-        isAMatch = false;
+      isAMatch = false;
     }
 
     // Check for Quality filter
     if (item.quality < filter.minQuality || item.quality > filter.maxQuality) {
       isAMatch = false;
     }
+  } else {
+    return false;
+  }
 
-    return isAMatch;
+  return isAMatch;
 }
